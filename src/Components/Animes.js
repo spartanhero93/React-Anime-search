edca52e751d696447cbb9aa3f3ac9bdd49c0d5fa
Luis Lopez <spartanhero93@gmail.com>
2018-02-19 20:02:47 -0500

    Fixed syntax errors

--- src/Components/Animes.js
+++ src/Components/Animes.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react'
-import Anime from './Anime';
+import React, { Component } from "react";
+import Anime from "./Anime";
 
-const Animes = (props) => {
-  console.log("Animes component called! " + props)
-  
-  if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-  return (
-    <div>
-      {props.data.map((item, index) => 
-      <Anime 
-        key={index}
-        item={item}/>
-      )
-      }
-    </div>
-  )
-}
+const Animes = props => {
+  console.log("Animes component called! " + props);
+
+  if (!props.isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div>
+        {props.data.map((item, index) => <Anime key={index} item={item} />)}
+      </div>
+    );
+  }
+};
 
 export default Animes;
